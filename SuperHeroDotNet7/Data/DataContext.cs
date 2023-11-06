83cabf41f18c464f38dad3e06e9fafbf324188a7
johannpa <johann_paque@hotmail.fr>
2023-11-06 20:08:39 +0100

    Created the Data's folder to have a DataContext

---
+++ SuperHeroDotNet7/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿global using Microsoft.EntityFrameworkCore;
+using SuperHeroDotNet7.Models;
+
+namespace SuperHeroDotNet7.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=superherodb1;Trusted_Connection=true;TrustServerCertificate=true;");
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
